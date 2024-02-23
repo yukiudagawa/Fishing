67a22f6730860a3b8143bfbad8b258a4af796017
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-02-23 10:12:53 +0800

    Display Message when catching fish

--- Assets/Script/DisplayMessage.cs
+++ Assets/Script/DisplayMessage.cs
@@ -7,21 +7,31 @@ public class DisplayMessage : MonoBehaviour
 {
     [SerializeField] private Texture image;
     private RawImage parentImage;
-    float totalTime = 5;
+    private Color temp;
+    public CatchingFish catchingFish;
+    [SerializeField] private GameObject fishGameObject;
+    private CatchingFish fishScript;
     // Start is called before the first frame update
     void Start()
     {
         parentImage = GetComponent<RawImage>();
+        temp = parentImage.color;
+        GameObject obj = GameObject.Find("Fish");
+        fishScript = obj.GetComponent<CatchingFish>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        totalTime -= Time.deltaTime;
-        if (totalTime < 0)
+        if (fishScript.catchfish == 1)
         {
             parentImage.texture = image;
             parentImage.color = Color.white;
+        }
+        else
+        {
+            parentImage.texture = image;
+            parentImage.color = temp;
         }
     }
 }
