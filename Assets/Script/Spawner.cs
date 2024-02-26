c161d3032c4e20206a9399af853f2032f9326f3d
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-02-26 15:31:53 +0800

    Fixed clouds and birds

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -8,24 +8,24 @@ public class Spawner : MonoBehaviour
     [SerializeField] private GameObject[] listOfObjects;
     [SerializeField] private int totalObjects;
     [SerializeField] private int height;
-    private Collider areaCollider;
+    [SerializeField] private int leastX;
+    [SerializeField] private int leastZ;
     private GameObject[] spawnedObjects;
 
     // Start is called before the first frame update
     void Start()
     {
-        areaCollider = GetComponent<Collider>();
         spawnedObjects = new GameObject[totalObjects];
 
         for (int i = 0; i < totalObjects; i++)
         {
-            Vector3 randomStartPosition = new Vector3(Random.Range(areaCollider.bounds.min.x, areaCollider.bounds.max.x), height, Random.Range(areaCollider.bounds.min.z, areaCollider.bounds.max.z));
+            Vector3 randomStartPosition = new Vector3(Random.Range(leastX, leastX + 688), height, Random.Range(leastZ, leastZ + 648));
             spawnedObjects[i] = Instantiate(listOfObjects[Random.Range(0, listOfObjects.Length)], randomStartPosition, Quaternion.Euler(new Vector3(0, 90, 0)));
         }
     }
 
-    private void OnTriggerExit(Collider other)
+ /*   private void OnTriggerExit(Collider other)
     {
-        other.transform.SetPositionAndRotation(new Vector3(areaCollider.bounds.min.x, height, Random.Range(areaCollider.bounds.min.z, areaCollider.bounds.max.z)), Quaternion.Euler(new Vector3(0, 90, 0)));
-    }
+        other.transform.SetPositionAndRotation(new Vector3(leastX, height, Random.Range(leastZ, leastZ + 648)), Quaternion.Euler(new Vector3(0, 90, 0)));
+    }*/
 }
