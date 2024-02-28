916a5acf4968d24a7bc5596d242f4b17341a5534
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-02-29 00:08:31 +0800

    Negative score implemented

--- Assets/Script/DisplayMessage.cs
+++ Assets/Script/DisplayMessage.cs
@@ -19,6 +19,7 @@ public class DisplayMessage : MonoBehaviour
     {
         parentImage = GetComponent<RawImage>();
         temp = parentImage.color;
+        ScoreViewmodel.totalFish = 2;
         //GameObject obj = GameObject.Find("Fish");
         //fishScript = obj.GetComponent<CatchingFish>();
     }
@@ -26,17 +27,30 @@ public class DisplayMessage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (CatchFishViewmodel.catchFishViewModel == 1)//catchingFish.catchfish == 1)
+        if (CatchFishViewmodel.catchFishViewModel == 1)
         {
             parentImage.texture = image[imageNum];
             parentImage.color = Color.white;
+
+            if (ran > 50)
+            {
+                ScoreViewmodel.totalFish -= 1;
+            }
+
+            if (ran > 50 && ScoreViewmodel.totalFish < 0)
+            {
+                scoreValue = -1;
+            }
+
             ScoreViewmodel.totalScore += scoreValue;
             scoreValue = 0;
+            ran = 0;
+
             Debug.Log(ScoreViewmodel.totalScore);
         }
         else
         {
-            ran = Random.Range(0, 100);
+            ran = Random.Range(51, 100);
             if (ran <= 10)
             {
                 imageNum = 3;
@@ -52,10 +66,21 @@ public class DisplayMessage : MonoBehaviour
                 imageNum = 1;
                 scoreValue = 2;
             }
-            else
+            else if(ran <= 100)
             {
                 imageNum = 0;
                 scoreValue = 1;
+/*                if (ScoreViewmodel.totalFish > 0)
+                {
+                    Debug.Log(ScoreViewmodel.totalFish);
+                    Debug.Log(scoreValue);
+                    ScoreViewmodel.totalFish -= 1;
+                }
+                else 
+                {
+                    Debug.Log("Less Fish: " + scoreValue);
+                    scoreValue = -1;
+                }*/
             }
             parentImage.color = temp;
         }
